d06c92d0c1a2a2be0ecc8fcd64a52adc85eca8a4
Jessica Kuijer <jessicakuijer@me.com>
2023-05-11 10:39:45 +0200

    ✨ fonctionnalité(style.css) : ajouter une modale pour ouvrir les liens travaux_public et travaux_particulier dans la même page ✨ fonctionnalité(final.js) : ajouter un événement pour ouvrir la modale pour les travaux publics La modale permet d'ouvrir les liens travaux_public et travaux_particulier dans la même page. Lorsque l'utilisateur clique sur le bouton "En savoir plus" pour les travaux publics, une modale s'ouvre pour afficher le contenu de la page travaux_public.html. Le fichier final.js a été modifié pour ajouter un événement qui ouvre la modale lorsque l'utilisateur clique sur le bouton "En savoir plus" pour les travaux publics.

--- js/final.js
+++ js/final.js
@@ -36,3 +36,29 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// modale pour les travaux publics et les travaux particuliers
+document.getElementById('btnTravauxPublics').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  var modal = document.getElementById('modal');
+  var modalBody = document.getElementById('modal-body');
+  var closeButton = document.querySelector('.close-button');
+
+  fetch('./travaux_public.html')
+  .then(response => response.text())
+  .then(data => {
+      modalBody.innerHTML = data;
+      modal.style.display = "block";
+  });
+
+  closeButton.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+});
